1a95dbd8d4845e50afd55b086a753320bf2097e1
Sneha Rani <sneharani1818@gmail.com>
2023-08-24 15:35:25 +0530

    Create lines.c

---
+++ Cprog/lines.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    printf("\"Hello\tEveryone\"\n");//this is a single line comment
+    printf("I am learning C!!");
+    /*this is 
+    a multi line comment*/
+    return 0;
+}
